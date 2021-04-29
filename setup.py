75b7f3bd189cb5202e7142eee0b7c75aaea1e6a3
virtualaizer <pont131995@gmail.com>
2021-04-29 20:15:04 +0500

    0.621

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='yandex_direct_stats',
     packages=['yandex_direct_stats'],
-    version='0.61',
+    version='0.621',
     license='MIT',
     description='Some tools for perform Yandex Direct reports',
     author='Danila Panteleev',
     author_email='pont131995@gmail.com',
     url='https://github.com/danila-panteleev/yandex_direct_stats',
-    download_url='https://github.com/danila-panteleev/yandex_direct_stats/archive/refs/tags/0.61.tar.gz',
+    download_url='https://github.com/danila-panteleev/yandex_direct_stats/archive/refs/tags/0.621.tar.gz',
     keywords=['Yandex Direct', 'Яндекс Директ'],
     install_requires=[
         'tapi-yandex-direct',
